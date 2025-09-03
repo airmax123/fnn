ffd4218bae0ff8c2488175605f983c00d264ef98
airmax123 <28534008+airmax123@users.noreply.github.com>
2025-09-03 11:37:19 +0200

    fit_a_sin: fix fn name, params usage

--- 02_fit_sin_curve.py
+++ 02_fit_sin_curve.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from fnn import *
 
-def fit_a_line():
+def fit_a_sin():
     np.set_printoptions(linewidth=160, precision=2, suppress=True)
 
     fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, gridspec_kw={'height_ratios':[80, 20]})
@@ -10,20 +10,20 @@ def fit_a_line():
     rng = np.random.default_rng(42)
 
     fnn_layers = [Layer(1, None, None),
-                  #Layer(32, tanh, tanh_prime),
-                  Layer(32, tanh, tanh_prime),
+                  Layer(16, tanh, tanh_prime),
+                  Layer(16, tanh, tanh_prime),
                   Layer(1, identity, identity_prime)]
 
     fnn = Fnn(w_init = Xavier_init, b_init = Xavier_init, layers = fnn_layers)
     
-    sample_count = 300
+    sample_count = 400
     X = np.random.uniform(-math.pi/4, math.pi/4, (sample_count, 1))
     T = 0.3 * np.sin(X / 0.1) + 0.4
     T += np.random.normal(0, 0.05, (sample_count, 1)) # Add small Gaussian noise to targets
 
     ax1.scatter(X, T, s = 30, marker = "^")
 
-    log = fnn.train(X, T, 2000, 240, 0.1, target_loss_diff = 0.000001)
+    log = fnn.train(X, T, 2000, 32, 0.01)
     
     X = np.random.uniform(-math.pi/4, math.pi/4, (sample_count, 1))
     Y, *_ = fnn.forward(X)
@@ -37,4 +37,4 @@ def fit_a_line():
     plt.show()
      
 if __name__ == '__main__':
-    fit_a_line()
+    fit_a_sin()
