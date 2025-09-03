ec78bbaf64a9b62d6cc80b9bc7ab678fb5170d49
airmax123 <28534008+airmax123@users.noreply.github.com>
2025-09-03 11:37:17 +0200

    Added Feedforward Neural Network with UnitTests

---
+++ fnn.py
@@ -0,0 +1,197 @@
+import random
+import math
+import numpy as np
+from collections import namedtuple
+import copy
+
+def as_column_vector(V):
+    return np.asarray(V)[:, np.newaxis]
+
+# Weight and bias init functions
+def zeros_init(n_in, n_out):
+    return np.zeros((n_in, n_out), dtype=float)
+
+def ones_init(n_in, n_out):
+    return np.ones((n_in, n_out), dtype=float)
+
+def arrange_init(n_in, n_out):
+    return np.arange(n_in, n_out + 1, 1, dtype=float)
+
+def rand_uniform_init(n_in, n_out):
+    return np.random.uniform(-0.1, 0.1, (n_in, n_out))
+
+def rand_norm_init(n_in, n_out):
+    return np.random.normal(0, 0.1, (n_in, n_out))
+
+def He_init(n_in, n_out):
+    return np.random.normal(0, math.sqrt(2 / n_in), (n_in, n_out))
+
+# Loss
+def loss_mean(T, Y):
+    L_per_sample = 1/2 * (T - Y)**2
+    return np.mean(L_per_sample)
+
+# Activation functions
+# For prime - use post-activation A, to avoid double calculations
+def identity(X):
+    return X
+
+def identity_prime(A):
+    return np.ones_like(A)
+
+def tanh(X):
+    return np.tanh(X)
+
+def tanh_prime(A):
+    return 1 - A**2
+
+# Feeedforward neural network
+class Layer:
+    def __init__(self, neurons, a_fn, a_fn_prime):
+        self.neurons = neurons
+        self.a_fn = a_fn
+        self.a_fn_prime = a_fn_prime
+
+# Batch-first network layout (PyTorch, TensorFlow, scikit-learn, NumPy) 
+# X: (batch_size, n_features_in)
+# Y: (batch_size, n_outputs)
+# W: (n_in, n_out)
+# b: (n_out,) <- 1-D vector. In such way NumPy broadcasts (n_out,) across rows
+# In such case math is: Z = A_prev @ W + b (in opposite to math formula Z = W @ A_prev + b)
+class Fnn:
+    def __init__(self, w_init, b_init, layers):
+        assert len(layers) >= 2, "Min amount of layers: input, output"
+        for layer in layers[1:]:
+            assert layer.a_fn != None and layer.a_fn_prime != None, "Every layer exept input should have defined activation function"
+
+        self.layers = layers
+        self.W = [w_init(layerN_minus1.neurons, layerN.neurons) for layerN_minus1, layerN in zip(layers[:-1], layers[1:])]
+        self.b = [b_init(1, layer.neurons).flatten() for layer in layers[1:]]
+    
+        self.best_W = []
+        self.best_b = []
+
+    def forward(self, X):
+        assert X.shape[1] == self.layers[0].neurons, "X should be column vector"
+        Z = []
+        A = []
+        A.append(X)
+
+        for b, W, layer in zip(self.b, self.W, self.layers[1:]):
+            Z.append( A[-1] @ W + b )
+            A.append( layer.a_fn(Z[-1]) )
+
+        A.pop(0)
+
+        return namedtuple("YZA", "Y Z A")(A[-1], Z, A)
+
+    def backprop(self, X, Z, A, T, eta):
+        dW = []
+        db = []
+        dZ = []
+
+        A.insert(0, X)
+        dL_dA = (A[-1] - T) / len(X)  # scale 1/B (mean over batch), (B, n_out)
+        
+        for i in range(len(self.layers[1:]) - 1, -1, -1):
+            a_prime = self.layers[i + 1].a_fn_prime
+
+            dA_dZ = a_prime(A[i + 1])   # (B, n_out) 
+            dL_dZ = dL_dA * dA_dZ   # multiply elementwise, (B, n_out)
+    
+            dZ_dW = A[i]
+            #dZ_db = np.array([1])
+            
+            dL_dW = dZ_dW.T @ dL_dZ # (n_in, n_out)
+            dL_db = dL_dZ.sum(axis=0)   # (n_out,)
+            
+            dL_dA = dL_dZ @ self.W[i].T  # (B, n_out) @ (n_out, n_in) -> (B, n_in)
+
+            self.W[i] -= eta * dL_dW
+            self.b[i] -= eta * dL_db
+            
+            dW.insert(0, dL_dW)
+            db.insert(0, dL_db)
+            dZ.insert(0, dL_dZ)
+
+        A.pop(0)
+
+        return dW, db, dZ
+    
+    def remember_best_state(self):
+        self.best_W = copy.deepcopy(self.W)
+        self.best_b = copy.deepcopy(self.b)
+
+    def revert_to_best_state(self):
+        if self.best_W and self.best_b:
+            self.W = copy.deepcopy(self.best_W)
+            self.b = copy.deepcopy(self.best_b)
+                
+    def train(self, X, T, max_epochs, batch_size, eta, eta_decay_rate = 0.98, target_loss_diff = 0.0001):
+        assert len(X) == len(T), "Size of X and T should be the same"
+        assert batch_size <= len(X), "batch_size should be smaller or same size as input X"
+        
+        train_log = []
+        
+        # Patience params
+        min_epochs = 200        
+        rel_tol = 1e-3 # relative improvement threshold
+        patience = 10 # epochs without improvement before stop
+        eps = 1e-12 # for numerical stability in relative test
+        L_eval_best = 1e+9 # set too far at the beggining
+        stale = 0
+
+        # Shuffle data
+        p = np.random.permutation(len(X))
+        X, T = X[p], T[p]
+        
+        # Split data 80/20
+        split_N = math.trunc(len(X) * 0.8)
+        X_train = X[: split_N]
+        T_train = T[: split_N]
+        X_eval = X[split_N: ]
+        T_eval = T[split_N: ]
+
+        for epoch in range(max_epochs):
+            p = np.random.permutation(len(X_train))
+            X, T = X_train[p], T_train[p]
+
+            X_batched = [X_train[i:i + batch_size] for i in range(0, len(X_train), batch_size)]        
+            T_batched = [T_train[i:i + batch_size] for i in range(0, len(T_train), batch_size)]        
+            
+            L_train = 0        
+            for X_batch, T_batch in zip(X_batched, T_batched):
+                X_batch = X_batch.reshape(-1, 1)
+                T_batch = T_batch.reshape(-1, 1)
+
+                Y_batch, Z_batch, A_batch = self.forward(X_batch)
+                self.backprop(X_batch, Z_batch, A_batch, T_batch, eta)
+
+                L_train += loss_mean(T_batch, Y_batch)
+            
+            # average over all mini-batches
+            L_train /= len(X)/batch_size
+            
+            # Compute eval loss
+            L_eval = loss_mean(T_eval, self.forward(X_eval).Y)
+            
+            # Relative improvement test
+            impr = (L_eval_best - L_eval) / (abs(L_eval_best) + eps)
+            if impr > rel_tol:            
+                self.remember_best_state()
+                L_eval_best = L_eval
+                stale = 0
+            else:
+                stale += 1
+            
+            train_log += [[L_train, L_eval, stale]]
+            
+            if epoch >= min_epochs and stale >= patience:
+                # we'll restore best state on exit from the loop
+                break
+
+            eta = eta * eta_decay_rate
+        
+        self.revert_to_best_state()
+        
+        return np.array(train_log)
---
+++ test_fnn.py
@@ -0,0 +1,304 @@
+import random
+import math
+import numpy as np
+import unittest
+from collections import namedtuple
+import copy
+from fnn import *
+
+# UnitTests
+class TestFnn(unittest.TestCase):
+    def test_fnn_loss_mean(self):
+        Y = as_column_vector([[1.0, 1.0], [2.0, 2.0], [3.0, 3.0], [4.0, 4.0]])
+        T = as_column_vector([[1.0, 1.0], [2.0, 2.0], [3.0, 3.0], [4.0, 4.0]])
+        
+        L = loss_mean(T, Y)
+
+        self.assertEqual(L, 0.0)
+
+    def test_fnn_W_b_shape(self):
+        features_X = 2
+        neurons_L1 = 3
+        neurons_L2 = 4
+        features_Y = 1
+        fnn_layers = [Layer(features_X, None, None),
+                      Layer(neurons_L1, identity, identity_prime), 
+                      Layer(neurons_L2, identity, identity_prime),
+                      Layer(features_Y, identity, identity_prime)]
+
+        fnn = Fnn(w_init = zeros_init, b_init = ones_init, layers = fnn_layers)
+
+        self.assertEqual(fnn.W[0].shape, (features_X, neurons_L1))
+        self.assertEqual(fnn.W[1].shape, (neurons_L1, neurons_L2))
+        self.assertEqual(fnn.W[2].shape, (neurons_L2, features_Y))
+
+        self.assertEqual(fnn.b[0].shape, (neurons_L1, ))
+        self.assertEqual(fnn.b[1].shape, (neurons_L2, ))
+        self.assertEqual(fnn.b[2].shape, (features_Y, ))
+
+    def test_fnn_1_1_1(self):
+        fnn_layers = [Layer(1, None, None),
+                      Layer(1, identity, identity_prime), 
+                      Layer(1, identity, identity_prime)]
+
+        fnn = Fnn(w_init = zeros_init, b_init = ones_init, layers = fnn_layers)
+
+        X = as_column_vector([1.0, 2.0, 3.0, 4.0])
+        T = as_column_vector([1.0, 1.0, 1.0, 1.0])
+        self.assertEqual(X.shape, T.shape)
+
+        Y, Z, A = fnn.forward(X)
+
+        self.assertEqual(Y.shape, T.shape)
+        self.assertTrue(np.allclose(Y, T))
+        
+        for z, a, layer in zip(Z, A, fnn_layers[1:]):
+            self.assertEqual(z.shape, (len(X), layer.neurons))
+            self.assertEqual(a.shape, z.shape)
+        
+        fnn.backprop(X, Z, A, T, 0.1)
+        
+        Y1 = fnn.forward(X).Y
+        T1 = as_column_vector([1.0, 1.0, 1.0, 1.0])        
+
+        self.assertTrue(np.allclose(Y1, T1))
+        
+    def test_fnn_1_3_1(self):
+        fnn_layers = [Layer(1, None, None),
+                      Layer(3, identity, identity_prime), 
+                      Layer(1, identity, identity_prime)]
+
+        fnn = Fnn(w_init = ones_init, b_init = ones_init, layers = fnn_layers)
+
+        X = as_column_vector([1.0,  2.0,  3.0,  4.0])
+        T = as_column_vector([7.0, 10.0, 13.0, 16.0])
+        
+        Y, Z, A = fnn.forward(X)
+
+        self.assertEqual(Y.shape, T.shape)
+        self.assertTrue(np.allclose(Y, T))
+
+        for z, a, layer in zip(Z, A, fnn_layers[1:]):
+            self.assertEqual(z.shape, (len(X), layer.neurons))
+            self.assertEqual(a.shape, z.shape)
+
+    def test_fnn_1_1_3_bias_broadcast(self):
+        fnn_layers = [Layer(1, None, None),
+                      Layer(1, identity, identity_prime), 
+                      Layer(3, identity, identity_prime)]
+
+        fnn = Fnn(w_init = zeros_init, b_init = arrange_init, layers = fnn_layers)
+
+        X = as_column_vector([1.0,  2.0,  3.0,  4.0])
+        T = np.array([[1.0, 2.0, 3.0], [1.0, 2.0, 3.0], [1.0, 2.0, 3.0], [1.0, 2.0, 3.0]])
+        
+        Y, Z, A = fnn.forward(X)
+
+        self.assertEqual(Y.shape, T.shape)
+        self.assertTrue(np.allclose(Y, T))
+
+        for z, a, layer in zip(Z, A, fnn_layers[1:]):
+            self.assertEqual(z.shape, (len(X), layer.neurons))
+            self.assertEqual(a.shape, z.shape)
+
+    def test_fnn_1_2_1_backprop_W(self):
+        random.seed(22)
+        np.random.seed(22)
+
+        fnn_layers = [Layer(1, None, None),
+                      Layer(2, identity, identity_prime), 
+                      Layer(1, identity, identity_prime)]
+        
+        fnn = Fnn(w_init = rand_norm_init, b_init = rand_norm_init, layers = fnn_layers)
+
+        X = as_column_vector([-0.5,  0.0, 0.5])
+        T = as_column_vector([ 0.0, 0.25, 0.5])
+        #X = as_column_vector([-0.5,  -0.5, -0.5])
+        #T = as_column_vector([ 0.0, 0.0, 0.0])
+        #X = as_column_vector([-0.5])
+        #T = as_column_vector([ 0.0])
+        
+        Y0, Z0, A0 = fnn.forward(X)
+        
+        epsilon = 0.000001
+        W_backup = fnn.W[1][0, 0]
+        
+        fnn.W[1][0, 0] += epsilon
+        Y_plus, *_ = fnn.forward(X)
+        fnn.W[1][0, 0] = W_backup
+
+        L_plus = loss_mean(T, Y_plus)
+
+        fnn.W[1][0, 0] -= epsilon
+        Y_minus, *_ = fnn.forward(X)
+        fnn.W[1][0, 0] = W_backup
+
+        L_minus = loss_mean(T, Y_minus)
+        
+        g_num = (L_plus - L_minus) / (2 * epsilon)        
+
+        dW, db, dZ = fnn.backprop(X, Z0, A0, T, 0.1)
+        
+        g_bp = dW[1][0,0]
+        self.assertTrue(math.isclose(g_bp, g_num))
+
+        abs_err = abs(g_num - g_bp)
+        rel_err = abs_err / max(1, abs(g_num), abs(g_bp))
+        self.assertTrue(abs_err < 0.00000001)
+        self.assertTrue(rel_err < 0.000001)
+
+    def test_fnn_1_2_1_backprop_b(self):
+        random.seed(22)
+        np.random.seed(22)
+
+        fnn_layers = [Layer(1, None, None),
+                      Layer(2, identity, identity_prime), 
+                      Layer(1, identity, identity_prime)]
+        
+        fnn = Fnn(w_init = rand_norm_init, b_init = rand_norm_init, layers = fnn_layers)
+
+        X = as_column_vector([-0.5,  0.0, 0.5])
+        T = as_column_vector([ 0.0, 0.25, 0.5])
+        
+        Y0, Z0, A0 = fnn.forward(X)
+        
+        epsilon = 0.000001
+        b_backup = fnn.b[1][0]
+        
+        fnn.b[1][0] += epsilon
+        Y_plus, *_ = fnn.forward(X)
+        fnn.b[1][0] = b_backup
+
+        L_plus = loss_mean(T, Y_plus)
+
+        fnn.b[1][0] -= epsilon
+        Y_minus, *_ = fnn.forward(X)
+        fnn.b[1][0] = b_backup
+
+        L_minus = loss_mean(T, Y_minus)
+        
+        g_num = (L_plus - L_minus) / (2 * epsilon)        
+
+        dW, db, dZ = fnn.backprop(X, Z0, A0, T, 0.1)
+        
+        g_bp = db[1][0]
+        self.assertTrue(math.isclose(g_bp, g_num))
+
+        abs_err = abs(g_num - g_bp)
+        rel_err = abs_err / max(1, abs(g_num), abs(g_bp))
+        self.assertTrue(abs_err < 0.00000001)
+        self.assertTrue(rel_err < 0.000001)
+
+    def test_fnn_1_2_1_backprop_indicies_correctness(self):
+        random.seed(22)
+        np.random.seed(22)
+
+        fnn_layers = [Layer(1, None, None),
+                      Layer(2, tanh, tanh_prime), 
+                      Layer(1, identity, identity_prime)]
+        
+        fnn = Fnn(w_init = rand_norm_init, b_init = rand_norm_init, layers = fnn_layers)
+
+        X = as_column_vector([-0.5,  0.0, 0.5])
+        T = as_column_vector([ 0.0, 0.25, 0.5])
+        
+        Y, Z, A = fnn.forward(X)
+        
+        A.insert(0, X)
+
+        dL_dA_out = (A[2] - T) / len(X)
+        dZ_out = dL_dA_out * fnn.layers[2].a_fn_prime(A[2])
+       
+        G_back = dZ_out @ fnn.W[1].T
+        G_gate = fnn.layers[1].a_fn_prime(A[1])
+
+        dZ_hidden_expected = G_back * G_gate
+        dW_hidden_expected = A[0].T @ dZ_hidden_expected
+
+        A.pop(0)
+
+        dW, db, dZ = fnn.backprop(X, Z, A, T, 0.1)
+        
+        self.assertTrue(np.allclose(dW[0], dW_hidden_expected))
+        self.assertTrue(np.allclose(dZ[1], dZ_out))        
+        self.assertTrue(np.allclose(dZ[0], dZ_hidden_expected))
+
+    def test_fnn_1_2_1_backprop_W_hid(self):
+        random.seed(22)
+        np.random.seed(22)
+
+        fnn_layers = [Layer(1, None, None),
+                      Layer(2, tanh, tanh_prime), 
+                      Layer(1, identity, identity_prime)]
+        
+        fnn = Fnn(w_init = rand_norm_init, b_init = rand_norm_init, layers = fnn_layers)
+
+        X = as_column_vector([-0.5,  0.0, 0.5])
+        T = as_column_vector([ 0.0, 0.25, 0.5])
+        
+        Y0, Z0, A0 = fnn.forward(X)
+        
+        epsilon = 0.000001
+        W_backup = fnn.W[0][0, 0]
+        
+        fnn.W[0][0, 0] += epsilon
+        Y_plus, *_ = fnn.forward(X)
+        fnn.W[0][0, 0] = W_backup
+
+        L_plus = loss_mean(T, Y_plus)
+
+        fnn.W[0][0, 0] -= epsilon
+        Y_minus, *_ = fnn.forward(X)
+        fnn.W[0][0, 0] = W_backup
+
+        L_minus = loss_mean(T, Y_minus)
+        
+        g_num = (L_plus - L_minus) / (2 * epsilon)        
+
+        dW, db, dZ = fnn.backprop(X, Z0, A0, T, 0.1)
+        
+        g_bp = dW[0][0,0]
+        self.assertTrue(math.isclose(g_bp, g_num))
+
+        abs_err = abs(g_num - g_bp)
+        rel_err = abs_err / max(1, abs(g_num), abs(g_bp))
+        self.assertTrue(abs_err < 0.00000001)
+        self.assertTrue(rel_err < 0.000001)
+
+    def test_fnn_1_2_1_one_step_SGD_lowers_loss(self):
+        random.seed(22)
+        np.random.seed(22)
+
+        fnn_layers = [Layer(1, None, None),
+                      Layer(2, tanh, tanh_prime), 
+                      Layer(1, identity, identity_prime)]
+        
+        fnn = Fnn(w_init = rand_norm_init, b_init = rand_norm_init, layers = fnn_layers)
+
+        X = as_column_vector([-0.5,  0.0, 0.5])
+        T = as_column_vector([ 0.0, 0.25, 0.5])
+        
+        Y, Z, A = fnn.forward(X)
+        
+        L0 = loss_mean(T, Y)
+        
+        dW, db, dZ = fnn.backprop(X, Z, A, T, 0.1)
+
+        W_copy = fnn.W
+        b_copy = fnn.b
+
+        alfa = 0.001
+        
+        for i in range(0, len(fnn.W) - 1):
+            fnn.W[i] -= alfa * dW[i]
+            fnn.b[i] -= alfa * db[i]
+
+        Y, Z, A = fnn.forward(X)
+
+        L1 = loss_mean(T, Y)
+
+        self.assertLess(L1, L0)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFnn)
+    unittest.TextTestRunner(verbosity=2).run(suite)
